874c699e47f33a410139a21d311bcf524a9ba50f
Ahmad <ahmad.yasarg@gmail.com>
2026-02-06 16:18:43 +0300

    sync

--- src/main/java/com/elm/expense_tracker_api/model/BaseEntity.java
+++ src/main/java/com/elm/expense_tracker_api/model/BaseEntity.java
@@ -3,6 +3,8 @@ package com.elm.expense_tracker_api.model;
 import java.time.Instant;
 
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,5 +16,15 @@ public class BaseEntity {
 
     private Instant createdAt; 
     private Instant updatedAt; 
+
+    @PrePersist
+    void onCreate() { 
+        this.createdAt = Instant.now(); 
+    }
+
+    @PreUpdate
+    void onUpdate(){ 
+        this.updatedAt = Instant.now(); 
+    }
 
 }
